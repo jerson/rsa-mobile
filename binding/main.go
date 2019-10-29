a2b05b9179622b3edb814b8dfa95d75569fe5f76
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2019-10-29 11:12:03 -0500

    added convert types for string

--- binding/main.go
+++ binding/main.go
@@ -2,66 +2,123 @@ package main
 
 import "C"
 import (
+	"fmt"
 	"github.com/jerson/rsa-mobile/rsa"
 )
 
+/*
+#include <stdlib.h>
+#include <string.h>
+*/
 var instance = rsa.NewFastRSA()
 
 //export DecryptOAEP
-func DecryptOAEP(ciphertext, label, hashName, pkcs12, passphrase string) (string, error) {
-	return instance.DecryptOAEP(ciphertext, label, hashName, pkcs12, passphrase)
+func DecryptOAEP(ciphertext, label, hashName, pkcs12, passphrase *C.char) *C.char {
+	result, err := instance.DecryptOAEP(C.GoString(ciphertext), C.GoString(label), C.GoString(hashName), C.GoString(pkcs12), C.GoString(passphrase))
+	if err != nil {
+		fmt.Println(err)
+		return C.CString("")
+	}
+	return C.CString(result)
 }
 
 //export DecryptPKCS1v15
-func DecryptPKCS1v15(ciphertext, pkcs12, passphrase string) (string, error) {
-	return instance.DecryptPKCS1v15(ciphertext, pkcs12, passphrase)
+func DecryptPKCS1v15(ciphertext, pkcs12, passphrase *C.char) *C.char {
+	result, err := instance.DecryptPKCS1v15(C.GoString(ciphertext), C.GoString(pkcs12), C.GoString(passphrase))
+	if err != nil {
+		fmt.Println(err)
+		return C.CString("")
+	}
+	return C.CString(result)
 }
 
 //export EncryptOAEP
-func EncryptOAEP(message, label, hashName, pkcs12, passphrase string) (string, error) {
-	return instance.EncryptOAEP(message, label, hashName, pkcs12, passphrase)
+func EncryptOAEP(message, label, hashName, pkcs12, passphrase *C.char) *C.char {
+	result, err := instance.EncryptOAEP(C.GoString(message), C.GoString(label), C.GoString(hashName), C.GoString(pkcs12), C.GoString(passphrase))
+	if err != nil {
+		fmt.Println(err)
+		return C.CString("")
+	}
+	return C.CString(result)
 }
 
 //export EncryptPKCS1v15
-func EncryptPKCS1v15(message, pkcs12, passphrase string) (string, error) {
-	return instance.EncryptPKCS1v15(message, pkcs12, passphrase)
+func EncryptPKCS1v15(message, pkcs12, passphrase *C.char) *C.char {
+	result, err := instance.EncryptPKCS1v15(C.GoString(message), C.GoString(pkcs12), C.GoString(passphrase))
+	if err != nil {
+		fmt.Println(err)
+		return C.CString("")
+	}
+	return C.CString(result)
 }
 
 //export Generate
-func Generate(nBits int) (map[string]string, error) {
+func Generate(nBits int) map[*C.char]*C.char {
 	result, err := instance.Generate(nBits)
 	if err != nil {
-		return nil, err
+		fmt.Println(err)
+		return nil
 	}
-	return map[string]string{
-		"publicKey":  result.PublicKey,
-		"privateKey": result.PrivateKey,
-	}, err
+	return map[*C.char]*C.char{
+		C.CString("publicKey"):  C.CString(result.PublicKey),
+		C.CString("privateKey"): C.CString(result.PrivateKey),
+	}
 }
 
 //export Hash
-func Hash(message, name string) (string, error) {
-	return instance.Hash(message, name)
+func Hash(message, name *C.char) *C.char {
+	result, err := instance.Hash(C.GoString(message), C.GoString(name))
+	if err != nil {
+		fmt.Println(err)
+		return C.CString("")
+	}
+	return C.CString(result)
 }
 
 //export SignPKCS1v15
-func SignPKCS1v15(message, hashName, pkcs12, passphrase string) (string, error) {
-	return instance.SignPKCS1v15(message, hashName, pkcs12, passphrase)
+func SignPKCS1v15(message, hashName, pkcs12, passphrase *C.char) *C.char {
+	result, err := instance.SignPKCS1v15(C.GoString(message), C.GoString(hashName), C.GoString(pkcs12), C.GoString(passphrase))
+	if err != nil {
+		fmt.Println(err)
+		return C.CString("")
+	}
+	return C.CString(result)
 }
 
 //export SignPSS
-func SignPSS(message, hashName, pkcs12, passphrase string) (string, error) {
-	return instance.SignPSS(message, hashName, pkcs12, passphrase)
+func SignPSS(message, hashName, pkcs12, passphrase *C.char) *C.char {
+	result, err := instance.SignPSS(C.GoString(message), C.GoString(hashName), C.GoString(pkcs12), C.GoString(passphrase))
+	if err != nil {
+		fmt.Println(err)
+		return C.CString("")
+	}
+	return C.CString(result)
 }
 
 //export VerifyPKCS1v15
-func VerifyPKCS1v15(signature, message, hashName, pkcs12, passphrase string) (bool, error) {
-	return instance.VerifyPKCS1v15(signature, message, hashName, pkcs12, passphrase)
+func VerifyPKCS1v15(signature, message, hashName, pkcs12, passphrase *C.char) *C.char {
+	result, err := instance.VerifyPKCS1v15(C.GoString(signature), C.GoString(message), C.GoString(hashName), C.GoString(pkcs12), C.GoString(passphrase))
+	if err != nil {
+		fmt.Println(err)
+		return C.CString("")
+	}
+	if result {
+		return C.CString("1")
+	}
+	return C.CString("")
 }
 
 //export VerifyPSS
-func VerifyPSS(signature, message, hashName, pkcs12, passphrase string) (bool, error) {
-	return instance.VerifyPSS(signature, message, hashName, pkcs12, passphrase)
+func VerifyPSS(signature, message, hashName, pkcs12, passphrase *C.char) *C.char {
+	result, err := instance.VerifyPSS(C.GoString(signature), C.GoString(message), C.GoString(hashName), C.GoString(pkcs12), C.GoString(passphrase))
+	if err != nil {
+		fmt.Println(err)
+		return C.CString("")
+	}
+	if result {
+		return C.CString("1")
+	}
+	return C.CString("")
 }
 
 func main() {}
