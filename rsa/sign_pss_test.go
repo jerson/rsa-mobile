a1d0e2c7720631fdcebc5a87e3f6d30f166279c1
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2019-10-31 16:06:19 -0500

    removed extra code from test

--- rsa/sign_pss_test.go
+++ rsa/sign_pss_test.go
@@ -5,12 +5,8 @@ import "testing"
 func TestFastRSA_SignPSS(t *testing.T) {
 
 	instance := NewFastRSA()
-	hash, err := instance.Hash(inputMessage, "sha512")
-	if err != nil {
-		t.Fatal(err)
-	}
 
-	output, err := instance.SignPSS(hash, "sha512", p12, passphrase)
+	output, err := instance.SignPSS(inputMessage, "sha512", p12, passphrase)
 	if err != nil {
 		t.Fatal(err)
 	}
