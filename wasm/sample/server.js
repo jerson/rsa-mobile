a448b0e26f541ec2f292321477f408a9dc1a843f
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2020-02-23 17:56:23 -0500

    added cors

--- wasm/sample/server.js
+++ wasm/sample/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 express.static.mime.define({'application/wasm': ['wasm']});
 var app = express();
 
+app.use(cors());
 app.use('/', express.static('public'));
 
 app.listen(3000, function () {
