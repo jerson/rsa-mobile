00e3189a2fcc95b1b2a34ee910ff2e3acf5a464e
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2020-03-03 15:23:01 -0500

    updated bindings now using custom error

--- binding/main.go
+++ binding/main.go
@@ -1,24 +1,27 @@
 package main
 
-import "C"
-import (
-	"fmt"
-
-	"github.com/jerson/rsa-mobile/rsa"
-)
-
 /*
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
+
+void error(char *message) {
+   throw message;
+}
 */
+import "C"
+import (
+	"github.com/jerson/rsa-mobile/rsa"
+)
+
 var instance = rsa.NewFastRSA()
 
 //export DecryptOAEP
 func DecryptOAEP(ciphertext, label, hashName, pkcs12, passphrase *C.char) *C.char {
 	result, err := instance.DecryptOAEP(C.GoString(ciphertext), C.GoString(label), C.GoString(hashName), C.GoString(pkcs12), C.GoString(passphrase))
 	if err != nil {
-		fmt.Println(err)
-		return C.CString("")
+		C.error(C.CString(err.Error()))
+		return nil
 	}
 	return C.CString(result)
 }
@@ -27,8 +30,8 @@ func DecryptOAEP(ciphertext, label, hashName, pkcs12, passphrase *C.char) *C.cha
 func DecryptPKCS1v15(ciphertext, pkcs12, passphrase *C.char) *C.char {
 	result, err := instance.DecryptPKCS1v15(C.GoString(ciphertext), C.GoString(pkcs12), C.GoString(passphrase))
 	if err != nil {
-		fmt.Println(err)
-		return C.CString("")
+		C.error(C.CString(err.Error()))
+		return nil
 	}
 	return C.CString(result)
 }
@@ -37,8 +40,8 @@ func DecryptPKCS1v15(ciphertext, pkcs12, passphrase *C.char) *C.char {
 func EncryptOAEP(message, label, hashName, pkcs12, passphrase *C.char) *C.char {
 	result, err := instance.EncryptOAEP(C.GoString(message), C.GoString(label), C.GoString(hashName), C.GoString(pkcs12), C.GoString(passphrase))
 	if err != nil {
-		fmt.Println(err)
-		return C.CString("")
+		C.error(C.CString(err.Error()))
+		return nil
 	}
 	return C.CString(result)
 }
@@ -47,8 +50,8 @@ func EncryptOAEP(message, label, hashName, pkcs12, passphrase *C.char) *C.char {
 func EncryptPKCS1v15(message, pkcs12, passphrase *C.char) *C.char {
 	result, err := instance.EncryptPKCS1v15(C.GoString(message), C.GoString(pkcs12), C.GoString(passphrase))
 	if err != nil {
-		fmt.Println(err)
-		return C.CString("")
+		C.error(C.CString(err.Error()))
+		return nil
 	}
 	return C.CString(result)
 }
@@ -57,7 +60,7 @@ func EncryptPKCS1v15(message, pkcs12, passphrase *C.char) *C.char {
 func Generate(nBits int) map[*C.char]*C.char {
 	result, err := instance.Generate(nBits)
 	if err != nil {
-		fmt.Println(err)
+		C.error(C.CString(err.Error()))
 		return nil
 	}
 	return map[*C.char]*C.char{
@@ -70,8 +73,18 @@ func Generate(nBits int) map[*C.char]*C.char {
 func Hash(message, name *C.char) *C.char {
 	result, err := instance.Hash(C.GoString(message), C.GoString(name))
 	if err != nil {
-		fmt.Println(err)
-		return C.CString("")
+		C.error(C.CString(err.Error()))
+		return nil
+	}
+	return C.CString(result)
+}
+
+//export Base64
+func Base64(message *C.char) *C.char {
+	result, err := instance.Base64(C.GoString(message))
+	if err != nil {
+		C.error(C.CString(err.Error()))
+		return nil
 	}
 	return C.CString(result)
 }
@@ -80,8 +93,8 @@ func Hash(message, name *C.char) *C.char {
 func SignPKCS1v15(message, hashName, pkcs12, passphrase *C.char) *C.char {
 	result, err := instance.SignPKCS1v15(C.GoString(message), C.GoString(hashName), C.GoString(pkcs12), C.GoString(passphrase))
 	if err != nil {
-		fmt.Println(err)
-		return C.CString("")
+		C.error(C.CString(err.Error()))
+		return nil
 	}
 	return C.CString(result)
 }
@@ -90,8 +103,8 @@ func SignPKCS1v15(message, hashName, pkcs12, passphrase *C.char) *C.char {
 func SignPSS(message, hashName, pkcs12, passphrase *C.char) *C.char {
 	result, err := instance.SignPSS(C.GoString(message), C.GoString(hashName), C.GoString(pkcs12), C.GoString(passphrase))
 	if err != nil {
-		fmt.Println(err)
-		return C.CString("")
+		C.error(C.CString(err.Error()))
+		return nil
 	}
 	return C.CString(result)
 }
@@ -100,8 +113,8 @@ func SignPSS(message, hashName, pkcs12, passphrase *C.char) *C.char {
 func VerifyPKCS1v15(signature, message, hashName, pkcs12, passphrase *C.char) *C.char {
 	result, err := instance.VerifyPKCS1v15(C.GoString(signature), C.GoString(message), C.GoString(hashName), C.GoString(pkcs12), C.GoString(passphrase))
 	if err != nil {
-		fmt.Println(err)
-		return C.CString("")
+		C.error(C.CString(err.Error()))
+		return nil
 	}
 	if result {
 		return C.CString("1")
@@ -113,8 +126,8 @@ func VerifyPKCS1v15(signature, message, hashName, pkcs12, passphrase *C.char) *C
 func VerifyPSS(signature, message, hashName, pkcs12, passphrase *C.char) *C.char {
 	result, err := instance.VerifyPSS(C.GoString(signature), C.GoString(message), C.GoString(hashName), C.GoString(pkcs12), C.GoString(passphrase))
 	if err != nil {
-		fmt.Println(err)
-		return C.CString("")
+		C.error(C.CString(err.Error()))
+		return nil
 	}
 	if result {
 		return C.CString("1")
