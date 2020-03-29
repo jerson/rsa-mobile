dbc80d78888f5238fd681347121c49aa1e7f070a
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2020-03-29 11:26:32 -0500

    added generation for cert

--- rsa/convert_keypair_pkcs12.go
+++ rsa/convert_keypair_pkcs12.go
@@ -2,9 +2,13 @@ package rsa
 
 import (
 	"crypto/rand"
+	cryptoRSA "crypto/rsa"
 	"crypto/x509"
+	"crypto/x509/pkix"
 	"encoding/pem"
+	"math/big"
 	"software.sslmate.com/src/go-pkcs12"
+	"time"
 )
 
 func (r *FastRSA) ConvertKeyPairToPKCS12(privateKey, publicKey, certificate, passphrase string) (string, error) {
@@ -26,8 +30,18 @@ func (r *FastRSA) ConvertKeyPairToPKCS12(privateKey, publicKey, certificate, pas
 		}
 	}
 
-	certificateBlock, _ := pem.Decode([]byte(certificate))
-	certificateCert, err := x509.ParseCertificate(certificateBlock.Bytes)
+	var certificateBytes []byte
+	if certificate == "" {
+		certificateBytes, err = createDefaultCertificate(privateKeyCert)
+		if err != nil {
+			return "", err
+		}
+	} else {
+		certificateBlock, _ := pem.Decode([]byte(certificate))
+		certificateBytes = certificateBlock.Bytes
+	}
+
+	certificateCert, err := x509.ParseCertificate(certificateBytes)
 	if err != nil {
 		return "", err
 	}
@@ -43,4 +57,33 @@ func (r *FastRSA) ConvertKeyPairToPKCS12(privateKey, publicKey, certificate, pas
 	}
 
 	return string(output), nil
+}
+
+func createDefaultCertificate(privateKeyCert *cryptoRSA.PrivateKey) ([]byte, error) {
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return nil, err
+	}
+	notBefore := time.Now()
+	notAfter := notBefore.Add(time.Hour * 24 * 365)
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{""},
+		},
+		NotBefore: notBefore,
+		NotAfter:  notAfter,
+
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+
+	certificateCreated, err := x509.CreateCertificate(rand.Reader, &template, &template, &privateKeyCert.PublicKey, privateKeyCert)
+	if err != nil {
+		return nil, err
+	}
+
+	return certificateCreated, nil
 }
--- rsa/convert_keypair_pkcs12_test.go
+++ rsa/convert_keypair_pkcs12_test.go
@@ -35,3 +35,32 @@ func TestFastRSA_ConvertKeyPairToPKCS12Complete(t *testing.T) {
 	encoded, _ := json.MarshalIndent(output, "", " ")
 	t.Log("output:", string(encoded))
 }
+
+func TestFastRSA_ConvertKeyPairToPKCS12WithoutCert(t *testing.T) {
+	instance := NewFastRSA()
+	output, err := instance.ConvertKeyPairToPKCS12(privateKey, publicKey, "", passphrase)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("output:", output)
+}
+
+func TestFastRSA_ConvertKeyPairToPKCS12CompleteWithoutCert(t *testing.T) {
+	instance := NewFastRSA()
+	pkcs12, err := instance.ConvertKeyPairToPKCS12(privateKey, publicKey, "", passphrase)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	output, err := instance.ConvertPKCS12ToKeyPair(pkcs12, passphrase)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, publicKey, output.PublicKey)
+	assert.Equal(t, privateKey, output.PrivateKey)
+
+	encoded, _ := json.MarshalIndent(output, "", " ")
+	t.Log("output:", string(encoded))
+}
