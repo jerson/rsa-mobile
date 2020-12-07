4e73063af46df2a6e544879dea77d8239d97b211
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2020-12-06 22:22:02 -0500

    added wasm support using bytes

---
+++ wasm/sample/public/caller.js
@@ -0,0 +1,52 @@
+const myWorker = new Worker('worker.js');
+
+const sample = async () => {
+
+    let pbf = new Pbf();
+    GenerateRequest.write({
+        nBits: 1024
+    }, pbf)
+    const buf = pbf.finish()
+    console.log('request', buf);
+    const rawResponse = await send('generate', buf)
+
+    const response = KeyPairResponse.read(new Pbf(rawResponse))
+    if (response.error) {
+        throw new Error(response.error)
+    }
+    console.log('response', response.output);
+}
+
+let counter = 0;
+const send = (name, request) => {
+    counter++;
+    const id = counter.toString()
+
+    return new Promise((resolve, reject) => {
+
+        const callbackError = (e) => {
+            reject('callbackError: ' + e)
+        }
+        const callbackMessageError = (e) => {
+            reject('callbackMessageError: ' + e)
+        }
+        const callback = (e) => {
+            const data = e.data || {}
+            if (id !== data.id) {
+                // if not same if we should not reject
+                return
+            }
+            myWorker.removeEventListener('message', callback)
+            const {error, response} = data;
+            if (error) {
+                reject(error)
+            }
+            resolve(response);
+        }
+
+        myWorker.addEventListener('message', callback)
+        myWorker.addEventListener('error', callbackError)
+        myWorker.addEventListener("messageerror", callbackMessageError)
+        myWorker.postMessage({id, name, request});
+    })
+}
---
+++ wasm/sample/public/worker.js
@@ -0,0 +1,43 @@
+self.importScripts('wasm_exec.js');
+
+let loaded = false;
+load = () => {
+    const go = new Go();
+    let mod, inst;
+    return WebAssembly.instantiateStreaming(fetch("rsa.wasm"), go.importObject).then(
+        async result => {
+            mod = result.module;
+            inst = result.instance;
+            loaded = true
+            const run = async () => {
+                try {
+                    await go.run(inst);
+                } catch (e) {
+                    console.warn(e)
+                    loaded = false
+                    await load()
+                }
+            }
+            run()
+        }
+    );
+}
+
+onmessage = async ({data}) => {
+    if (!loaded) {
+        await load();
+    }
+
+    const {request, name, id} = data
+
+    rsaBridgeCall(name, request, request.length, (error, response) => {
+
+        const payload = {
+            id,
+            response,
+            error,
+        }
+
+        postMessage(payload);
+    })
+}
